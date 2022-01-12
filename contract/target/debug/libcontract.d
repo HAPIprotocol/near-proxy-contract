b8773f4764fcba5a6dfc5f811e25cbd0d33107fe
Serhii Rudniev <serhii.rudniev@hapi.one>
2022-01-12 18:29:00 +0200

    added contract

---
+++ contract/target/debug/libcontract.d
@@ -0,0 +1,1 @@
+/Users/serhiirudniev/Documents/Work/hapi/projects/hapi-proxy-near/contract/target/debug/libcontract.rlib: /Users/serhiirudniev/Documents/Work/hapi/projects/hapi-proxy-near/contract/src/lib.rs
